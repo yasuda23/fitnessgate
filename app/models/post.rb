6ffefcfa91e59508d68aba8eb73668a3a592808f
yasuda23 <ysuda11412@gmail.com>
2024-05-29 12:21:29 +0900

    ER図作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_one_attached :video
   validates :title, presence: true
   validates :workout_details, presence: true
 end
