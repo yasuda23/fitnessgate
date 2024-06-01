1404c97ce0057d15b127e092c23bb7bee390d6f3
yasuda23 <ysuda11412@gmail.com>
2024-06-01 10:00:15 +0900

    修正

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -1,8 +1,8 @@
 window.addEventListener('turbo:load', function(){
-  const articles = document.querySelectorAll(".row");
+  const articles = document.querySelectorAll(".shadow");
   articles.forEach(function(article) {
     article.addEventListener('mouseover', function() {
-      this.setAttribute("style", "box-shadow: -50px -10px 20px -10px rgba(0, 0, 0, 0.5);");
+      this.setAttribute("style", "box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.3);");
     });
     article.addEventListener('mouseout', function() {
       this.removeAttribute("style");
